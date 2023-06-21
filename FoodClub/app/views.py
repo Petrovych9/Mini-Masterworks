b7c4a821c1d4629f91577c5305d12f500835debd
Bohdan <bogdan.pogr@gmail.com>
2023-06-21 11:09:01 +0300

    views

--- FoodClub/app/views.py
+++ FoodClub/app/views.py
@@ -4,7 +4,7 @@ mainBlueprint = Blueprint('main', __name__)
 adminBlueprint = Blueprint('admin', __name__)
 authBlueprint = Blueprint('authentication', __name__)
 
-menu = ['Home', 'New recipe', 'Profile', ]
+menu = ['Home', 'New recipe', 'Profile', '1111' ]
 
 
 #main
