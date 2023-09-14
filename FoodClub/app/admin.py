6cc9bff38b1aaba3d6920dadae0aa97f6f10b23d
Bohdan <bohdan.work@urk.net>
2023-09-14 10:03:19 +0300

    update structure

--- FoodClub/app/admin.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-adminBlueprint = Blueprint('admin', __name__)
--- FoodClub/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ajnjfgfhfn!1tyuiop[92vgb9r5'
